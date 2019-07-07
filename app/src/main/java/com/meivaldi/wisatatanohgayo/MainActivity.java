d10529653c0063d930ef07dcc6a6f55b48a6b96d
meivaldi <meivaldi@gmail.com>
2019-07-07 19:46:45 +0700

    Set starter

--- app/src/main/java/com/meivaldi/wisatatanohgayo/MainActivity.java
+++ app/src/main/java/com/meivaldi/wisatatanohgayo/MainActivity.java
@@ -1,11 +1,27 @@
 package com.meivaldi.wisatatanohgayo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.meivaldi.wisatatanohgayo.fragment.DiscoverFragment;
+import com.meivaldi.wisatatanohgayo.fragment.HomeFragment;
+import com.meivaldi.wisatatanohgayo.fragment.LoginFragment;
+import com.meivaldi.wisatatanohgayo.fragment.ProfileFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +30,45 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loadFragment(new HomeFragment());
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    loadFragment(new HomeFragment());
+                    return true;
+                case R.id.navigation_explore:
+                    loadFragment(new DiscoverFragment());
+                    return true;
+                case R.id.navigation_profile:
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    if (user != null)
+                        loadFragment(new ProfileFragment());
+                    else
+                        loadFragment(new LoginFragment());
+
+                    break;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.commit();
+    }
+
 }
